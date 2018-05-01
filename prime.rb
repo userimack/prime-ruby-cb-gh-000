92e69c8cd2a2ab31a1a1c8edf4c48c83072745a9
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-01 06:14:15 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,11 @@
 # Add  code here!
 def prime?(number)
+  if 
   counter = 0 
   while counter < Math.sqrt(number).to_i
     if number/counter == 0
-      return true 
+      return true
+    counter += 1
     end 
   end 
   false 
